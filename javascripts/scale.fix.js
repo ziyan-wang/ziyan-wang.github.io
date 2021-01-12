cff1748a4c5e423d903a7c79a805d3fb0211b507
ziyan-wang <77264699+ziyan-wang@users.noreply.github.com>
2021-01-12 18:44:54 +0800

    Update scale.fix.js

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -34,7 +34,7 @@ const currentTheme = localStorage.getItem("theme");
 if (currentTheme == "light") {
   document.body.classList.toggle("light-theme");
   switchToLightMode();
-} else if (currentTheme == "dark" || prefersDarkScheme.matches) {
+} else if (currentTheme == "dark") {
   document.body.classList.toggle("dark-theme");
   switchToDarkMode();
 }
