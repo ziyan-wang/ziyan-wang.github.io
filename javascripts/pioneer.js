e559b4f14282ff0a068e439ef51229ca59d39462
ziyan-wang <77264699+ziyan-wang@users.noreply.github.com>
2021-06-29 22:47:50 +0800

    fix bugs

--- javascripts/pioneer.js
+++ javascripts/pioneer.js
@@ -3,8 +3,8 @@
 
   function rejectUnsupportedBrowsers() {
     // does not support IE
+    // WARNING: cannot use window.stop() in IE
     if (window.navigator.userAgent.match(/(MSIE|Trident)/) !== null) {
-      window.stop();
       window.location.replace('unsupported.html');
     }
   }
